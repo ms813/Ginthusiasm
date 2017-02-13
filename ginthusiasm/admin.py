565006f12abea9af9d653276b5ab86d1acc01abe
= <matthew.smith813@gmail.com>
2017-02-13 21:58:05 +0000

    Splitting models out into separate files. Adding basic user model. Adding shell population script

--- ginthusiasm/admin.py
+++ ginthusiasm/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from ginthusiasm.models import Article, Distillery, Gin, Review, UserProfile, Wishlist
+
+admin.site.register(Article)
+admin.site.register(Distillery)
+admin.site.register(Gin)
+admin.site.register(Review)
+admin.site.register(UserProfile)
+admin.site.register(Wishlist)
