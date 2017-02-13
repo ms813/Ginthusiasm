10463a28ed25325377885d47162ef48223246ee1
= <matthew.smith813@gmail.com>
2017-02-13 18:30:40 +0000

    Defining all models as shells, so that other models can reference them as OneToMany fields etc

--- ginthusiasm/models.py
+++ ginthusiasm/models.py
@@ -2,4 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Gin(model.Models):
+
+class Article(model.Models):
+
+class Review(model.Models):
+
+class Distillery(model.Models):
+
+class UserProfile(models.Model):    
+
+class Wishlist(models.Model):
