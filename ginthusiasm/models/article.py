4d496e0939810454d22b7cffe636598394bb407c
Catherine Daly <catherinedaly@glaroam2-165-100.wireless.gla.ac.uk>
2017-02-21 10:06:55 +0000

    Adding in my article model

--- ginthusiasm/models/article.py
+++ ginthusiasm/models/article.py
@@ -1,6 +1,19 @@
 from __future__ import unicode_literals
 from django.db import models
+from user_profile import UserProfile
 
 class Article(models.Model):
-    # pass just lets this class be empty, remove it when the model is implemented
-    pass
+
+    title = models.CharField(max_length=128, unique=True)
+    shortDesc = models.TextField(max_length=1000, unique=True)
+    content = models.TextField()
+    date = models.DateField()
+    slug = models.SlugField(max_length=50)
+    # Not sure what should be in the foreign key bracket??
+    author = models.ForeignKey('UserProfile', related_name='article')
+
+    class Meta:
+        verbose_name_plural = 'Articles'
+
+    def __str__(self):
+        return self.title
