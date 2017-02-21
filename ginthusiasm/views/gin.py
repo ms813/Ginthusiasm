ae4ca31cda123e6f416622ee6ad1ee5126d8e0d3
= <2022607g@student.gla.ac.uk>
2017-02-21 10:10:25 +0000

    added show_gin view for the main gin page

--- ginthusiasm/views/gin.py
+++ ginthusiasm/views/gin.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from ginthusiasm.models import Gin
 
+# View for the main gin page
 def show_gin(request, gin_name_slug):
     context_dict = {}
 
