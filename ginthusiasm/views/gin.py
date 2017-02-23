d396ef254f07db7b76082b74d09af65854091fd2
= <2022607g@student.gla.ac.uk>
2017-02-23 18:13:49 +0000

    Added advanced search functionality to the gin_search_page - still needs refining

--- ginthusiasm/views/gin.py
+++ ginthusiasm/views/gin.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from ginthusiasm.models import Gin
+from ginthusiasm.forms import AdvancedSearchForm
 from django.db.models import Q
 
 # View for the main gin page
@@ -21,11 +22,49 @@ def show_gin(request, gin_name_slug):
     return render(request, 'ginthusiasm/gin_page.html', context=context_dict)
 
 def gin_search_results(request):
+    query_dict = request.GET
 
-    query = Q(price__gt=30.00)
+    print query_dict
+    queries = Q()
 
-    gin_list = Gin.objects.filter(query).order_by('-average_rating')
+    # Build filter query
+    if query_dict.get('keywords'):
+        queries.add
+        (
+            (
+                Q(name__icontains=query_dict.get('keywords')) |
+                Q(short_description__icontains=query_dict.get('keywords')) |
+                Q(long_description__icontains=query_dict.get('keywords'))
+            ), Q.AND
+        )
+    if query_dict.get('max_price'):
+        queries.add
+        (
+            (
+                Q(price__lt=query_dict.get('max_price')) |
+                Q(price=query_dict.get('max_price'))
+            ), Q.AND
+        )
+    if query_dict.get('min_price'):
+        queries.add
+        (
+            (
+                Q(price__gt=query_dict.get('min_price')) |
+                Q(price=query_dict.get('min_price'))
+            ), Q.AND
+        )
 
-    context_dict = {'gins': gin_list}
+    order_by = ''
+    # The order defaults to ascending
+    if query_dict.get('order') == 'DESC':
+        order_by = '-'
+
+    if query_dict.get('order_by'):
+        order_by += query_dict.get('order_by')
+
+    # Execute filter query
+    gin_list = Gin.objects.filter(queries).order_by(order_by)
+
+    context_dict = {'gins': gin_list, 'advanced_search_form': AdvancedSearchForm()}
 
     return render(request, 'ginthusiasm/gin_search_page.html', context=context_dict)
