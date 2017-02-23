08a6f7bfa175a7babd31f345541550ef74ed20cb
Catherine Daly <catherinedaly@glaroam2-133-167.wireless.gla.ac.uk>
2017-02-23 10:39:01 +0000

    Adding in Article model and views

--- ginthusiasm/admin.py
+++ ginthusiasm/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from ginthusiasm.models import Article, Distillery, Gin, Review, UserProfile, Wishlist
+from ginthusiasm.models import Article, Distillery, Gin, TasteTag, Review, UserProfile, Wishlist
 
 admin.site.register(Article)
 admin.site.register(Distillery)
 admin.site.register(Gin)
+admin.site.register(TasteTag)
 admin.site.register(Review)
 admin.site.register(UserProfile)
 admin.site.register(Wishlist)
