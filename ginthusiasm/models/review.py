8bd77a0076ca2376925f7c9af52bae6cd0d27379
rhetaylor <r.taylor.code@gmail.com>
2017-02-25 13:48:28 +0000

    Reviews model added

--- ginthusiasm/models/review.py
+++ ginthusiasm/models/review.py
@@ -1,6 +1,43 @@
 from __future__ import unicode_literals
+from django.template.defaultfilters import slugify
 from django.db import models
 
 class Review(models.Model):
-    # pass just lets this class be empty, remove it when the model is implemented
-    pass
+    # Single characters means less spaced used in DB
+    EXPERT = 'e'
+    USER = 'u'
+
+    # Tuple of tuples makes these choices immutable (can also be used to represent a single row from a database)
+    # Maps user selection to variables above
+    REVIEW_TYPE_CHOICES = (
+        (EXPERT, 'Expert review'),
+        (USER, 'User review'),
+    )
+
+    review_type = models.CharField(
+        max_length=1,
+        choices = REVIEW_TYPE_CHOICES,
+        default = USER
+    )
+
+    date = models.DateField()
+    rating = models.PositiveSmallIntegerField()
+    summary = models.CharField(max_length=1024)
+    content = models.TextField()
+    lat = models.FloatField(blank=False)
+    long = models.FloatField(blank=False)
+    slug = models.SlugField(unique=True)
+    # Assuming that when a user deletes their profile they'll want all their
+    # reviews deleted too. Not sure about the foreign key argument?
+    user = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    gin = models.ForeignKey('Gin', on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Review, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
