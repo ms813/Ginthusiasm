08852455cd9e84b74c8403b67e28cbe7a52aafca
= <matthew.smith813@gmail.com>
2017-02-28 18:19:07 +0000

    Fixing merge conflict in gin_data.json

--- populate_db.py
+++ populate_db.py
@@ -174,7 +174,7 @@ def populate_gin():
             "distillery" : ""
         }
     ]"""
-    print(gins)
+    # print(gins)
     for data in gins:
         gin, created = Gin.objects.get_or_create(name = data['name'])
 
