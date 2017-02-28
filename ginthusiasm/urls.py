6530fb0764ea28e4155a211c63bf49276fb3169d
catherine-daly <catherine.f.daly@gmail.com>
2017-02-28 17:51:51 +0000

    Fixed the article urls

--- ginthusiasm/urls.py
+++ ginthusiasm/urls.py
@@ -57,7 +57,8 @@ review_patterns = [
 ##########     Article     ##########
 article_patterns = [
     url(r'^article/$', views.article_listing, name='article_listing'),
-    url(r'^article/(?P<article_name_slug>[\w\-]+)/$', views.article, name ='article'),
+    url(r'^article/(?P<user_name>[\w\-]+)/$', views.article_user_listing, name='article_user_listing'),
+    url(r'^article/(?P<user_name>[\w\-]+)/(?P<article_name_slug>[\w\-]+)/$', views.article, name ='article'),
 ]
 
 ##########     Debug     ##########
--- ginthusiasm/views/article.py
+++ ginthusiasm/views/article.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from ginthusiasm.models import Article
+from ginthusiasm.models import UserProfile
+from django.contrib.auth.models import User
 
 #View for the main article page
-def article(request, article_name_slug):
+def article(request, article_name_slug, user_name):
 
     context_dict={}
+    check = User.objects.get(username=user_name).userprofile
 
     try:
-        article = Article.objects.get(slug=article_name_slug)
+        article = Article.objects.get(slug=article_name_slug, author=check)
         context_dict['article'] = article
         print(article)
 
@@ -19,10 +22,21 @@ def article(request, article_name_slug):
     return render(request, 'ginthusiasm/article.html', context = context_dict)
 
 def article_listing(request):
+
     context_dict = {}
 
-    articles = Article.objects.filter()
-    context_dict['article'] = articles
+    article = Article.objects.filter()
+    context_dict['article'] = article
+
+    # Go render the response and return it to the client.
+    return render(request, 'ginthusiasm/article_listing.html', context_dict)
+
+def article_user_listing(request, user_name):
+    context_dict = {}
+    check = User.objects.get(username=user_name).userprofile
+    article = Article.objects.filter(author =check)
+
+    context_dict['article'] = article
 
     # Go render the response and return it to the client.
     return render(request, 'ginthusiasm/article_listing.html', context_dict)
