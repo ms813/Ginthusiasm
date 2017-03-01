4298fe0975617f1326ac408a69b32211d68daac5
rhetaylor <r.taylor.code@gmail.com>
2017-03-01 22:49:56 +0000

    Adding review dummy data to population script

--- populate_db.py
+++ populate_db.py
@@ -61,8 +61,6 @@ def populate_article():
         "month": False,
 
     }
-
-
     ]
 
     for data in articles:
@@ -214,7 +212,77 @@ def populate_gin():
 
 def populate_review():
     print("Populating reviews...")
-    # review population here...
+    user = User.objects.get(username="Rozz")
+    userprofile = user.userprofile
+    gin = Gin.objects.get(name="Eden Mill Love Gin")
+
+    reviews = [
+        {
+            "review_type" : Review.EXPERT,
+            "date" : '2017-03-01',
+            "rating" : 3,
+            "summary" : "Cupcake ipsum dolor sit amet. Wafer macaroon biscuit chupa chups candy canes candy chocolate cake chocolate. Chocolate cake chupa chups liquorice lemon drops sweet roll cheesecake jelly pudding. Carrot cake cupcake lemon drops candy canes powder sugar plum wafer marzipan.",
+            "content" : "Pudding jelly chocolate cake lollipop cupcake. Candy cotton candy pie sweet lollipop. Souffle cheesecake danish halvah. Muffin brownie powder pastry. Candy sweet roll jujubes jelly. Pie icing icing chupa chups lemon drops bear claw carrot cake muffin chocolate bar. Cheesecake bonbon icing lollipop sweet caramels powder. Powder croissant candy lemon drops. Bonbon brownie marzipan gingerbread candy bear claw powder tart. Donut candy sesame snaps. Halvah cake sweet apple pie. Cake oat cake tiramisu cake. Toffee dragee croissant jelly beans dragee macaroon chocolate cake tootsie roll.",
+            "lat" : 51.503351,
+            "long" : -0.119522,
+            "user" : userprofile,
+            "gin" : gin
+        },
+        {
+            "review_type": Review.EXPERT,
+            "date": '2017-02-28',
+            "rating": 2,
+            "summary": "Cupcake ipsum dolor sit amet. Wafer macaroon biscuit chupa chups candy canes candy chocolate cake chocolate. Chocolate cake chupa chups liquorice lemon drops sweet roll cheesecake jelly pudding. Carrot cake cupcake lemon drops candy canes powder sugar plum wafer marzipan.",
+            "content": "Pudding jelly chocolate cake lollipop cupcake. Candy cotton candy pie sweet lollipop. Souffle cheesecake danish halvah. Muffin brownie powder pastry. Candy sweet roll jujubes jelly. Pie icing icing chupa chups lemon drops bear claw carrot cake muffin chocolate bar. Cheesecake bonbon icing lollipop sweet caramels powder. Powder croissant candy lemon drops. Bonbon brownie marzipan gingerbread candy bear claw powder tart. Donut candy sesame snaps. Halvah cake sweet apple pie. Cake oat cake tiramisu cake. Toffee dragee croissant jelly beans dragee macaroon chocolate cake tootsie roll.",
+            "lat": 51.504493,
+            "long": -0.086500,
+            "user": userprofile,
+            "gin": gin
+        },
+        {
+            "review_type": Review.USER,
+            "date": '2017-02-27',
+            "rating": 4,
+            "summary": "Cupcake ipsum dolor sit amet. Wafer macaroon biscuit chupa chups candy canes candy chocolate cake chocolate. Chocolate cake chupa chups liquorice lemon drops sweet roll cheesecake jelly pudding. Carrot cake cupcake lemon drops candy canes powder sugar plum wafer marzipan.",
+            "content": "Pudding jelly chocolate cake lollipop cupcake. Candy cotton candy pie sweet lollipop. Souffle cheesecake danish halvah. Muffin brownie powder pastry. Candy sweet roll jujubes jelly. Pie icing icing chupa chups lemon drops bear claw carrot cake muffin chocolate bar. Cheesecake bonbon icing lollipop sweet caramels powder. Powder croissant candy lemon drops. Bonbon brownie marzipan gingerbread candy bear claw powder tart. Donut candy sesame snaps. Halvah cake sweet apple pie. Cake oat cake tiramisu cake. Toffee dragee croissant jelly beans dragee macaroon chocolate cake tootsie roll.",
+            "lat": 51.513839,
+            "long": -0.098351,
+            "user": userprofile,
+            "gin": gin
+        },
+        {
+            "review_type": Review.USER,
+            "date": '2017-02-26',
+            "rating": 1,
+            "summary": "Cupcake ipsum dolor sit amet. Wafer macaroon biscuit chupa chups candy canes candy chocolate cake chocolate. Chocolate cake chupa chups liquorice lemon drops sweet roll cheesecake jelly pudding. Carrot cake cupcake lemon drops candy canes powder sugar plum wafer marzipan.",
+            "content": "Pudding jelly chocolate cake lollipop cupcake. Candy cotton candy pie sweet lollipop. Souffle cheesecake danish halvah. Muffin brownie powder pastry. Candy sweet roll jujubes jelly. Pie icing icing chupa chups lemon drops bear claw carrot cake muffin chocolate bar. Cheesecake bonbon icing lollipop sweet caramels powder. Powder croissant candy lemon drops. Bonbon brownie marzipan gingerbread candy bear claw powder tart. Donut candy sesame snaps. Halvah cake sweet apple pie. Cake oat cake tiramisu cake. Toffee dragee croissant jelly beans dragee macaroon chocolate cake tootsie roll.",
+            "lat": 51.500723,
+            "long": -0.124636,
+            "user": userprofile,
+            "gin": gin
+        }
+    ]
+
+    for data in reviews:
+        reviews_results = []
+        try:
+            reviews_results = Review.objects.get(date=data['date'])
+        except Review.DoesNotExist:
+            pass
+
+        if not reviews_results:
+            review = Review()
+            review.review_type = data['review_type']
+            review.date = data['date']
+            review.rating = data['rating']
+            review.summary = data['summary']
+            review.content = data['content']
+            review.lat = data['lat']
+            review.long = data['long']
+            review.user = data['user']
+            review.gin = data['gin']
+
+            review.save()
 
 def populate_wishlist():
     print("Populating wishlist...")
@@ -323,8 +391,19 @@ if __name__ == '__main__':
     if len(sys.argv) <= 1:
         # if no command line arguments, populate every model
         # note first cmd arg is 'populate_db.py'
-        for key in models:
-            models[key]();
+
+        # RT - had to comment out the loop below, and add
+        # these functions here to make sure DB was populating in
+        # right order
+        populate_users()
+        populate_distillery()
+        populate_gin()
+        populate_review()
+        populate_wishlist()
+        populate_article()
+
+        # for key in models:
+        #     models[key]();
         print("Successfully finished populating all models")
     else:
         # populate each model as specified from the command line arguments
