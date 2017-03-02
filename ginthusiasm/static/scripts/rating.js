dd798c30e6ac554e0b4bfddd359c6eaddcdb932d
RobertGilmour <2022607g@student.gla.ac.uk>
2017-03-02 12:58:56 +0000

    Added static and user enterable star ratings WORK IN PROGRESS

---
+++ ginthusiasm/static/scripts/rating.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+
+    $('.rating_widget').each(function(i, e){
+
+        console.log (e)
+
+        var average_rating = $(e).find('.average_rating');
+        var user_rating = $(e).find('.user_rating');
+
+        average_rating.barrating({
+            theme: 'fontawesome-stars-o',
+            initialRating: average_rating.data('rating'),
+            readonly: true,
+        });
+
+        user_rating.barrating({
+            theme: 'fontawesome-stars-o',
+            initialRating: user_rating.data('rating'),
+            onSelect: ratingClicked,
+        });
+    });
+});
+
+var ratingClicked = function(value, text, event) {
+    if (value === "") {
+        value = 0;
+    }
+
+    var data = $(event.target).closest('.rating_widget').data()
+
+    var request = $.post('/gin/' + data.gin + '/rate/', {rating: value});
+
+    request.done(function(data, status, jqXHR) {
+        console.log (data)
+    });
+    request.fail(function(data, status, jqXHR){});
+
+}
