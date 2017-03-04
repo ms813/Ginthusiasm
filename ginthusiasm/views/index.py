9647503e5e686fb4e9721848caaa5ccf59421cba
rhetaylor <r.taylor.code@gmail.com>
2017-03-04 15:43:10 +0000

    Setting up age verification

--- ginthusiasm/views/index.py
+++ ginthusiasm/views/index.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'ginthusiasm/index.html')
+    # Find out whether the user is over 18.
+    # We use the COOKIES.get() function to obtain the 'verified' cookie.
+    # If the cookie exists, the value returned is casted to a boolean.
+    # If the cookie does not exist, then the default value 'False' is used.
+    verified_age = bool(request.COOKIES.get('verified', False))
+    return render(request, 'ginthusiasm/index.html', {'verified_age': verified_age})
