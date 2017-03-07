ea86d7a5076c021c3655ad2a383d260103ef6086
= <matthew.smith813@gmail.com>
2017-03-07 20:37:25 +0000

    Adding some more user tests

--- ginthusiasm/tests/test_user.py
+++ ginthusiasm/tests/test_user.py
@@ -1,28 +1,53 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib import auth
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
-from ginthusiasm.models import UserProfile
+from ginthusiasm.models import UserProfile, Wishlist
+
 
 class UserTestCase(TestCase):
     def setUp(self):
-        user1 = User.objects.create(username="jsmith")
-        user1.first_name = "John"
-        user1.last_name = "Smith"
-        user1.email = "test@test.com"
-        user1.password = make_password("jsmith123")
+        self.user = User.objects.create(username="jsmith")
+        self.user.first_name = "John"
+        self.user.last_name = "Smith"
+        self.user.email = "test@test.com"
+        self.user.password = make_password("jsmith123")
 
-        user1.save()
+        self.user.save()
 
-        profile = UserProfile(user=user1, user_type=UserProfile.BASIC)
-        profile.save()
+        self.profile = UserProfile(user=self.user, user_type=UserProfile.BASIC)
+        self.profile.save()
 
+        self.client = Client()
 
     def test_user_exists(self):
-        user = User.objects.get(username="jsmith")
-        self.assertIsNotNone(user, "User not null")
-        profile = user.userprofile
+        self.assertIsNotNone(self.user, "User not null")
+        profile = self.user.userprofile
         self.assertIsNotNone(profile, "Profile not null")
 
-    def test_user_has_wishlist(self):
-        wishlist = User.objects.get(username="jsmith").userprofile.wishlist
-        self.assertIsNotNone(wishlist, "Has wishlist")
+    def test_user_data(self):
+        self.assertEqual(self.user.username, "jsmith", "Username match")
+        self.assertEqual(self.user.first_name, "John", "User first_name match")
+        self.assertEqual(self.user.last_name, "Smith", "User last_name match")
+        self.assertEqual(self.user.email, "test@test.com", "User email match")
+
+    def test_user_login(self):
+        response = self.client.post(reverse("login"), {"username" : "jsmith", "password" : "jsmith123"} )
+        self.assertRedirects(response, '/my-account/', msg_prefix="Redirect after user login")
+
+    def test_user_logout(self):
+        self.client.force_login(self.user)
+
+        user = auth.get_user(self.client)
+        self.assertTrue(user.is_authenticated(), "User is authenticated")
+
+        self.client.logout()
+        user = auth.get_user(self.client)
+        self.assertFalse(user.is_authenticated(), "User not authenticated after log out")
+
+
+
+
+
+
