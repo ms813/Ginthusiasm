eac468185a85ffd6da661c1c472e064e5d3c3b8e
= <matthew.smith813@gmail.com>
2017-03-09 14:53:18 +0000

    Reverting removal of comments

--- ginthusiasm/models/distillery.py
+++ ginthusiasm/models/distillery.py
@@ -8,6 +8,7 @@ Model representing a Distillery
 Gins are associated with a single Distillery
 """
 
+
 class Distillery(models.Model):
     name = models.CharField(max_length=225, unique=True, blank=True)
     address = models.CharField(max_length=225, blank=True)
@@ -19,9 +20,18 @@ class Distillery(models.Model):
     lat = models.FloatField(blank=True)
     long = models.FloatField(blank=True)
 
+    # *args, **kwargs = a way of specifying that a method/function can take
+    # extra parameters depending on future use (e.g. if need to save a Distillery
+    # in a specific way with something models.Model provides). *args = arguments, and
+    # **kwargs = keyword arguments. E.g. could call save(<extra parameters>)
+    # which would then get passed to the save method on the models.Model class. Means
+    # do not need to define lots of different save methods.
+
     # override the save method to automatically set the slug based on the name
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
+        # Get super class of Distillery (models.Model) & call its save method, and
+        # pass *args and **kwargs that were passed to this class's save method above.
         super(Distillery, self).save(*args, **kwargs)
 
     def __str__(self):
