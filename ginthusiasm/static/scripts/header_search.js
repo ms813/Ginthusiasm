a0e73d861dfc54dc01185c7808b9180cb92acb28
= <matthew.smith813@gmail.com>
2017-03-10 16:23:49 +0000

    Adding auto complete search results disappearing on blur

--- ginthusiasm/static/scripts/header_search.js
+++ ginthusiasm/static/scripts/header_search.js
@@ -1,42 +1,52 @@
 $(document).ready(() => {
-    $('#header-search-button').click(headerSearch);
 
-    /*$('.header-search').focusout(e => {
-        $('#header-search-results').hide()
+    let input = $('#header-search-field');
+    let results = $('#header-search-results');
+
+    //do the search when the search button is pressed
+    $('#header-search-button').click(e => {
+        headerSearch(e, input.val());
     });
-    $('.header-search').focusin(e => {
-        $('#header-search-results').show()
-    });*/
+
+    //if the search box is un-focused, hide the autocomplete results after a short delay
+    input.blur(e => {
+        setTimeout(() => {
+            results.hide()
+        }, 2000);
+    });
+
+    //show the autocomplete results when the search box is focused
+    input.focus(e => {
+        results.show();
+    });
 
     // if the cursor is in the header search bar, bind the enter key to search
-    $('#header-search-field').keyup(e => {
-        if(e.which === 13){
+    input.keyup(e => {
+        if (e.which === 13) {
+            // 13 is the keycode for enter
             headerSearch(e);
         } else {
-            var request = $.post('/gin-search/', {
-                    search_text : $('#header-search-field').val(),
-                    csrfmiddlewaretoken : $("input[name=csrfmiddlewaretoken]").val()
+            let request = $.post('/gin-search/', {
+                    search_text: input.val(),
+                    //csrfmiddlewaretoken : $("input[name=csrfmiddlewaretoken]").val()
                 }
-            )
+            );
 
-            request.done(function(data, textStatus, jqXHR) {
-                console.log(data)
-                $('#header-search-results').html(data);
-
+            request.done((data, textStatus, jqXHR) => {
+                results.html(data);
             });
         }
     });
 });
 
-var headerSearch = function(e) {
-    // grab the string from the search box
-    var inputString = $('#header-search-field').val();
+const headerSearch = function (e, inputString) {
 
     // remove punctuation and whitespace, replace spaces with +
-    var keywords = inputString.replace(/[^\w\s]/g, "").replace(/\s+/g, "+");
+    let keywords = inputString.replace(/[^\w\s]/g, "").replace(/\s+/g, "+");
 
     // make a GET request for the search url
     window.location = "/gin/?keywords=" + keywords;
 
+    //prevent the <a> tag from being followed
     return false;
-}
+};
