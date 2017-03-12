f8a9cdd124e0a30b16f6f1f5fe1a4949fb2ac735
rhetaylor <r.taylor.code@gmail.com>
2017-03-12 20:47:57 +0000

    Hook into social authentication flow to create User and UserProfile

--- ginthusiasm_project/settings.py
+++ ginthusiasm_project/settings.py
@@ -137,6 +137,19 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 LOGIN_REDIRECT_URL = 'index'
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'ginthusiasm.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
 SOCIAL_AUTH_TWITTER_KEY = 'kteQCTZyTTaqowswIgB8FtBhP'
 SOCIAL_AUTH_TWITTER_SECRET = 'plC5t9lZshdq2kydPuaUHSdPHVgvJEEzr02r8qwzS7oBcbXjkU'
 
