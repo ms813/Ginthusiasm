3dba79ef1138178524851209027ce218aa36ba5d
rhetaylor <r.taylor.code@gmail.com>
2017-03-12 17:14:34 +0000

    Updating index view and css for collection highlights widget

--- ginthusiasm/views/index.py
+++ ginthusiasm/views/index.py
@@ -1,3 +1,6 @@
+import random
+
+from django.db.models import Count, F
 from django.shortcuts import render
 
 from ginthusiasm.models.gin import Gin
@@ -16,9 +19,15 @@ def index(request):
     # If the cookie does not exist, then the default value 'False' is used.
     verified_age = bool(request.COOKIES.get('verified', False))
     trending_gins = Gin.objects.order_by('-average_rating')[:6]
+    collection_highlights = Gin.objects.annotate(tag_name=F("taste_tags__name")).values("tag_name").annotate(count=Count("taste_tags")).order_by("-count")[:3]
+
+    for tag_name in collection_highlights:
+        gins_with_tag = Gin.objects.filter(taste_tags__name=tag_name['tag_name'])
+        tag_name['img'] = random.choice(gins_with_tag).image.url
 
     context_dict = {
         'verified_age': verified_age,
         'trending_gins': trending_gins,
+        'collection_highlights': collection_highlights,
     }
     return render(request, 'ginthusiasm/index.html', context_dict)
