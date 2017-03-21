325d55feed0e5a3987326a837a2158dbdbea797c
rhetaylor <r.taylor.code@gmail.com>
2017-03-21 23:50:41 +0000

    Tidy imports and comments

--- ginthusiasm/views/index.py
+++ ginthusiasm/views/index.py
@@ -16,8 +16,8 @@ Includes multiple template widgets, requiring data from many models
 def index(request):
     # Find out whether the user is over 18.
     # We use the COOKIES.get() function to obtain the 'verified' cookie.
-    # If the cookie exists, the value returned is casted to a boolean.
-    # If the cookie does not exist, then the default value 'False' is used.
+    # If the cookie exists, the value returned is cast to a boolean.
+    # If the cookie does not exist, then the default value False is used.
     verified_age = bool(request.COOKIES.get('verified', False))
     trending_gins = Gin.objects.order_by('-average_rating')[:6]
     collection_highlights = Gin.objects.annotate(tag_name=F("taste_tags__name")).values("tag_name").annotate(count=Count("taste_tags")).order_by("-count")[:3]
@@ -31,6 +31,6 @@ def index(request):
         'verified_age': verified_age,
         'trending_gins': trending_gins,
         'collection_highlights': collection_highlights,
-        'gin_month':gin_month
+        'gin_month':gin_month,
     }
     return render(request, 'ginthusiasm/index.html', context_dict)
